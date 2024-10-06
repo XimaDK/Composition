2c164c73e4d6d07038b5906d03c41437b34350c0
XimaDK <kadyshev_dima_2@mail.ru>
2024-10-06 22:38:24 +0700

    add realisation data layer and resources ready

---
+++ app/src/main/java/kadyshev/dmitry/composition/data/GameRepositoryImpl.kt
@@ -0,0 +1,70 @@
+package kadyshev.dmitry.composition.data
+
+import kadyshev.dmitry.composition.domain.entity.GameSettings
+import kadyshev.dmitry.composition.domain.entity.Level
+import kadyshev.dmitry.composition.domain.entity.Question
+import kadyshev.dmitry.composition.domain.repository.GameRepository
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
+
+
+object GameRepositoryImpl : GameRepository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 1
+
+    override fun generateQuestion(maxSumValue: Int, countOfOption: Int): Question {
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxSumValue + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer - countOfOption, MIN_ANSWER_VALUE)
+        val to = min(maxSumValue, rightAnswer + countOfOption)
+        while (options.size < countOfOption) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Question(sum = sum, visibleNumber = visibleNumber, options.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        return when (level) {
+            Level.TEST -> {
+                GameSettings(
+                    maxSumValue = 10,
+                    minCountOfRightAnswers = 3,
+                    minPercentOfRightAnswers = 50,
+                    gameTimeInSeconds = 8
+                )
+            }
+
+            Level.EASY -> {
+                GameSettings(
+                    maxSumValue = 10,
+                    minCountOfRightAnswers = 10,
+                    minPercentOfRightAnswers = 70,
+                    gameTimeInSeconds = 60
+                )
+            }
+
+            Level.NORMAL -> {
+                GameSettings(
+                    maxSumValue = 20,
+                    minCountOfRightAnswers = 20,
+                    minPercentOfRightAnswers = 80,
+                    gameTimeInSeconds = 40
+                )
+            }
+
+            Level.HARD -> {
+                GameSettings(
+                    maxSumValue = 30,
+                    minCountOfRightAnswers = 30,
+                    minPercentOfRightAnswers = 90,
+                    gameTimeInSeconds = 40
+                )
+            }
+        }
+    }
+}
